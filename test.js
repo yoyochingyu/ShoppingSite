b2195889f15632ee074d4d5af13191ee7ba517e8
ching <yoyochingyu@gmail.com>
2020-07-02 18:05:10 +0800

    add policy & test googleOauth

--- test.js
+++ test.js
@@ -1,6 +1,40 @@
-var fruits = ["Strawberry","Banana","Mango"]
-var removedItem = fruits.splice(0, 1); 
-console.log(removedItem);  
-console.log(fruits.length);
-console.log(fruits);
-// ["Strawberry", "Mango"]
+const express = require("express"),
+      app = express(),
+      {google} = require('googleapis');
+
+app.set("view engine","ejs");
+// app.use(bodyParser.urlencoded({extended:true}));
+
+const YOUR_CLIENT_ID = '1032735084625-jvs02ge8mjsca6l7ll7f6or2j8fr9q34.apps.googleusercontent.com',
+        YOUR_CLIENT_SECRET = '1lMNz8cz21ReMFEwB_Xk6BFe',
+        YOUR_REDIRECT_URL = ["http://localhost:3000/oauth2callback","http://localhost:4000/oauth2callback","http://ec2-3-134-58-20.us-east-2.compute.amazonaws.com:4000/oauth2callback"];
+
+const scopes = [
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'openid'
+    ];
+
+const oauth2Client = new google.auth.OAuth2(
+    YOUR_CLIENT_ID,
+    YOUR_CLIENT_SECRET,
+    YOUR_REDIRECT_URL
+  );
+
+
+const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: scopes
+});
+
+app.get("/",(req,res)=>{
+    res.send("homepage");
+});
+app.get("/login",(req,res)=>{
+    res.render("user/googleOauth");
+});
+
+
+app.listen(3000,()=>{
+    console.log("server has started!");
+});
